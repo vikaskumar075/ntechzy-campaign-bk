2d6764a819ec25fa45deb2cbf26b7d79c5d2cb07
abhigyadufare <abhigyadufare@gmail.com>
2024-07-04 19:06:43 +0530

    cgsck

--- routes/link_route.js
+++ routes/link_route.js
@@ -2,7 +2,7 @@ import express from "express"
 import {save_medium} from "../controllers/link_controller.js";
 const Router = express.Router();
 Router.post("/save-medium",save_medium);
-Router.post("/",function name() {
+Router.get("/",function name() {
     console.log("working");
 });
 
