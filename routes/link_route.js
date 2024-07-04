09feccd5c7c8d42956e4ead41f4cda5ab0d0acd6
abhigyadufare <abhigyadufare@gmail.com>
2024-07-04 19:05:12 +0530

    c

--- routes/link_route.js
+++ routes/link_route.js
@@ -2,6 +2,10 @@ import express from "express"
 import {save_medium} from "../controllers/link_controller.js";
 const Router = express.Router();
 Router.post("/save-medium",save_medium);
+Router.post("/",function name() {
+    console.log("working");
+});
+
 
 
 export default Router; 
